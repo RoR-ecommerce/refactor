951582bf9c2260365a37c861f598688acd68d4ae
David Henner <drhenner@yahoo.com>
2013-04-22 19:59:35 -0700

    big change  oops.

--- refactor.rb
+++ refactor.rb
@@ -1,4 +1,4 @@
-# Controller
+# Controllers
 
 class People < ActionController::Base
 
@@ -12,15 +12,17 @@ class People < ActionController::Base
       render :new
     end
   end
+end
 
-  def validateEmail
+class PersonValidation < ActionController::Base
+  # move this to it's own controller..  I LIKE REST
+  def update
     @user = Person.find_by_slug(params[:slug])
     if @user.present?
-      @user.validated = true
-      @user.save
-      Rails.logger.info "USER: User ##{@person.id} validated email successfully."
-      Emails.admin_user_validated(@user.id)
-      Emails.welcome(@user.id).deliver!
+      @user.set_validated!
+    else
+      flash[:alert] = 'Sorry we could not process'
+      redirect_to sorry_email_validation_failed_url
     end
   end
 
@@ -46,8 +48,19 @@ class Person < ActiveRecord::Base
     "#{team}#{id}"
   end
 
+  # Move to Model.  Still I'd have to ask someone why we have this code in the first place
   def team
     id.odd? ? "UnicornRainbows" : "LaserScorpions"
+  end
+
+  def set_validated!
+    if !@user.validated
+      @user.update_attribute(:validated,  true)
+      # why is this here?
+      Rails.logger.info "USER: User ##{@person.id} validated email successfully."
+      Emails.admin_user_validated(@user.id)
+      Emails.welcome(@user.id).deliver
+    end
   end
 
   def self.admin_emails
@@ -56,6 +69,14 @@ class Person < ActiveRecord::Base
 
   def self.admin
     where(:admin => true)
+  end
+
+  def self.unvalidated
+    where(:validated => false)
+  end
+
+  def created_longer_than(days_ago )
+    where('created_at < ?', Time.zone.now - days_ago.days)
   end
 
   private
@@ -73,6 +94,8 @@ class Person < ActiveRecord::Base
   def set_slug
     self.slug = "ABC123#{Time.now.to_i.to_s}1239827#{rand(10000)}"
   end
+
+  # by default this is not and admin user
   def set_admin
     self.admin = false if admin.nil?
   end
@@ -82,33 +105,35 @@ end
 # Mailer
 
 class Emails < ActionMailer::Base
+  default :from => "foo@example.com"
 
   def welcome(person_id)
     @person = Person.find( person_id )
-    mail to: @person, from: 'foo@example.com'
+    mail to: @person.email
   end
 
   def validate_email(person_id)
     @person = Person.find( person_id )
-    mail to: @person.email, from: 'foo@example.com'
+    mail to: @person.email
   end
 
-  def admin_user_validated(admins, person_id)
+  def admin_user_validated(person_id)
     @admins = Person.admin_emails
-    @user = Person.find( person_id )
-    mail to: @admins, from: 'foo@example.com'
+    @user   = Person.find( person_id )
+    mail to: @admins
   end
 
   def admin_new_user(person_id)
     @admins = Person.admin_emails
-    @user = Person.find(person_id)
-    mail to: @admins, from: 'foo@example.com'
+    @user   = Person.find(person_id)
+    mail to: @admins
   end
 
-  def admin_removing_unvalidated_users(admins, users)
+  # best to move to a log file or something.  or even just inactivating the emails
+  def admin_removing_unvalidated_users(admins, user_emails)
     @admins = Person.admin_emails
-    @users = users
-    mail to: @admins, from: 'foo@example.com'
+    @user_emails  = user_emails
+    mail to: @admins
   end
 
 end
@@ -117,15 +142,16 @@ end
 # Rake Task
 
 namespace :accounts do
-
+  # rake accounts:remove_unvalidated --trace
   desc "Remove accounts where the email was never validated and it is over 30 days old"
   task :remove_unvalidated do
-    @people = Person.where('created_at < ?', Time.now - 30.days).where(:validated => false)
-    @people.each do |person|
+    # this is memory intensive...  best to move to a log file or something other than emailing to someone
+    @emails = Person.created_longer_than( 30 ).unvalidated.pluck(:email)
+    # find in batches...  memory HOG
+    Person.created_longer_than( 30 ).unvalidated.find_each do |person|
       Rails.logger.info "Removing unvalidated user #{person.email}"
-      person.destroy
+      person.destroy # i don't like destroying...  inactive please.
     end
-    Emails.admin_removing_unvalidated_users(Person.where(:admin => true), @people).deliver
+    Emails.admin_removing_unvalidated_users(Person.admin_emails, @emails).deliver
   end
-
 end
